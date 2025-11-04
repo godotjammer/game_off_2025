47b9ef2bce9ba6255ea2f2074bd360b344e91468
dglusak <godotjammer@gmail.com>
2025-11-04 10:41:43 -0500

    day 3

--- godot_proj/levels/level.gd
+++ godot_proj/levels/level.gd
@@ -1,1 +1,11 @@
 class_name Level extends Node3D
+
+@export var map: LevelMap
+
+func _ready() -> void:
+	_instance = self
+
+static var _instance: Level
+
+static func get_instance() -> Level:
+	return _instance
--- godot_proj/levels/tower_bases/tower_base.gd
+++
@@ -1,3 +0,0 @@
-class_name TowerBase extends Node3D
-
-var has_tower: bool
